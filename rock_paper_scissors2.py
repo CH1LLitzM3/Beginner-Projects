21f034cb9502106ad5375e2a37bd4f577d579fd5
Cameron <cam3r0nj1108@gmail.com>
2025-07-14 16:02:49 +0100

    Add files via upload

---
+++ rock_paper_scissors2.py
@@ -0,0 +1,50 @@
+import random
+from os import replace
+
+print("welcome to rock paper scissors\nquit using \"quit\"")
+while True:
+    exit_command = "quit"
+
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+
+    user_choice = input("you choose: ").lower()
+    user_nochoice = not rock or not paper or not scissors
+
+    comp_choice = random.choice([rock, paper, scissors])
+    print(f"computer chose: {comp_choice}")
+
+    if user_choice == comp_choice:
+        print("draw!")
+    elif user_choice == rock and comp_choice == scissors:
+        print("player wins!")
+    elif user_choice == paper and comp_choice == rock:
+        print("player wins!")
+    elif user_choice == scissors and comp_choice == paper:
+        print("player wins!")
+    elif comp_choice == rock and user_choice == scissors:
+        print("comp wins!")
+    elif comp_choice == paper and user_choice == rock:
+        print("comp wins!")
+    elif comp_choice == scissors and user_choice == paper:
+        print("comp wins!")
+    elif user_choice != exit_command or not rock or not paper or not scissors:
+        print("play properly :(")
+
+    if user_choice == "quit":
+        print("exit success")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
